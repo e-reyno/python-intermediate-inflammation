fa893b12d6d56a626c9b5c4e0dd6bb75aa5686c1
e-reyno <elliot.reynolds@stfc.ac.uk>
2022-11-02 12:47:18 +0000

    test patient normalise with 0 array

--- tests/test_models.py
+++ tests/test_models.py
@@ -52,3 +52,34 @@ def test_daily_min_string():
     
     with pytest.raises(TypeError):
         error_expected = daily_min([['abd', 'ads'], ['afs', 'dfs']])
+        
+@pytest.mark.parametrize(
+    "test, expected",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [0, 0, 0]),
+        ([[4, 2, 5], [1, 6, 2], [4, 1, 9]], [1, 1, 2]),
+        ([[4, -2, 5], [1, -6, 2], [-4, -1, 9]], [-4, -6, 2]),
+    ])
+def test_daily_min(test, expected):
+    """Test that min function works for an array of positive and negative integers."""
+    from inflammation.models import daily_min
+
+    npt.assert_array_equal(daily_min(test), expected)
+    
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], [[0, 0, 0], [0, 0, 0], [0, 0, 0]], None),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], [[1, 1, 1], [1, 1, 1], [1, 1, 1]], None),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]], None),
+        ([[-1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]], ValueError),
+    ]
+)
+def test_patient_normalise(test, expected, expect_raises):
+    '''Test normalisations works'''
+    from inflammation.models import patient_normalise
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
+    else:
+        npt.assert_almost_equal(patient_normalise(np.array(test)), np.array(expected), decimal=2)
